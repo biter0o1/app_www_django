8c95cb340d90cd657bfcdbd34cada761fa55f67a
biter0o1 <biter0o1@o2.pl>
2022-10-19 23:06:51 +0200

    Fix miesiace

--- Osoby/models.py
+++ Osoby/models.py
@@ -6,14 +6,14 @@ class Osoba(models.Model):
         ('2', 'Luty'),
         ('3', 'Marzec'),
         ('4', 'Kwiecień'),
-        (5, 'Maj'),
-        (6, 'Czerwiec'),
-        (7, 'Lipiec'),
-        (8, 'Sierpień'),
-        (9, 'Wrzesień'),
-        (10, 'Październik'),
-        (11, 'Listopad'),
-        (12, 'Grudzień'),
+        ('5', 'Maj'),
+        ('6', 'Czerwiec'),
+        ('7', 'Lipiec'),
+        ('8', 'Sierpień'),
+        ('9', 'Wrzesień'),
+        ('10', 'Październik'),
+        ('11', 'Listopad'),
+        ('12', 'Grudzień'),
     )
 
     imie = models.CharField(max_length=30, blank=False)
