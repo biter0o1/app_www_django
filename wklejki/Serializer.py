082332316c3863cb65ff62ff509396cf576950e9
biter0o1 <biter0o1@o2.pl>
2022-12-06 19:15:28 +0100

    Override update method

--- wklejki/Serializer.py
+++ wklejki/Serializer.py
@@ -7,6 +7,13 @@ class WklejkiSerializer(serializers.ModelSerializer):
     #kategoria = serializers.ChoiceField(source='get_kategoria_display', choices=Kategorie)
     #wlasciciel_wklejki = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     wlasciciel_wklejki = serializers.ReadOnlyField(source='wlasciciel_wklejki.username')
+
+    def update(self, instance, validated_data):
+        instance.tytul = validated_data.get('tytul', instance.tytul)
+        instance.tekst = validated_data.get('tekst', instance.tekst)
+        instance.kategoria = validated_data.get('kategoria', instance.kategoria)
+        instance.save()
+        return instance
     class Meta:
         model = Wklejki
         fields = '__all__'
